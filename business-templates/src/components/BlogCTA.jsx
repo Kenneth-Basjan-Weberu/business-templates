3c5b116aafc42ee5b073b8f2da7f584998096327
Kenneth Basjan <155391671+Kenneth-Basjan-Weberu@users.noreply.github.com>
2025-01-04 22:34:20 +0200

    Edited Pages and Components

---
+++ business-templates/src/components/BlogCTA.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlogCTA = () => {
+  return (
+    <div>BlogCTA</div>
+  )
+}
+
+export default BlogCTA
---
+++ business-templates/src/components/ServicesCTA.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ServicesCTA = () => {
+  return (
+    <div>ServicesCTA</div>
+  )
+}
+
+export default ServicesCTA
